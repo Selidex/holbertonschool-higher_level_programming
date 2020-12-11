169f2ee0a680f09f071bb2a5f8d8fde97cb87bd4
Selidex <justindparnell@yahoo.com>
2020-12-11 01:31:45 +0000

    Fixed test conditions

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) < 1:
+    if len(sys.argv) < 2:
         print("0 arguments.")
     else:
-        if len(sys.argv) is 1:
+        if len(sys.argv) is 2:
             print("1 argument:")
         else:
             print("{} arguments".format(len(sys.argv)-1))

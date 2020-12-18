7d16298df3e21790d2e6c72702f86febf1c3e7d8
Selidex <justindparnell@yahoo.com>
2020-12-18 02:46:32 +0000

    updating

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    ew_matrix = list(map(list, matrix))
-    for i in range(0, len(ew_matrix)):
-        for j in range(0, len(ew_matrix[i])):
-            ew_matrix[i][j] = (matrix[i][j] * matrix[i][j])
-    return ew_matrix
+    new_matrix = list(map(list, matrix))
+    for i in range(0, len(new_matrix)):
+        for j in range(0, len(new_matrix[i])):
+            new_matrix[i][j] = (matrix[i][j] * matrix[i][j])
+    return new_matrix

364b8634245f0dedef70478dd8eeccd74c7bdd40
Selidex <justindparnell@yahoo.com>
2021-01-05 16:04:23 -0600

    Added documentation for task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""This module shows initial value validation.
+"""
+
 class Square:
+    """This class shows value validation.
+    """
     def __init__(self, size=0):
+        """Square init.
+
+        Args:
+            size (int): the size of the square
+        """
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:

f85ad9fdb59c8174d5036013905185183a5d69da
Selidex <justindparnell@yahoo.com>
2021-01-05 21:47:20 +0000

    Adding documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+"""This module demonstrates the __init__ method
+when making classes
+"""
+
+
 class Square:
+    """This class shows the init method.
+    """
     def __init__(self, val):
+        """Square initializing method.
+
+        Args:
+            val (int): the size of the square
+        """
         self.__size = val

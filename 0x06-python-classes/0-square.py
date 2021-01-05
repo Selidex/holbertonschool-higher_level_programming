7570c64f60d5232344c6921f9f08728f71f6c943
Selidex <justindparnell@yahoo.com>
2021-01-05 21:43:51 +0000

    Added documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+""" This module contains an empty class to practice class
+declarations and definitons
+"""
+
+
 class Square:
+    """ This is an empty class.
+    """
     pass

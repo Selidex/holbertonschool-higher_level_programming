3b8e1a9cfb57b55379c32c1419f45c5e8ab1d19b
Selidex <justindparnell@yahoo.com>
2021-01-05 16:55:31 -0600

    Adding documentation

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+"""This module adds additional methods to the class
+"""
+
+
 class Square:
+    """This class possesses additional methods
+    """
     def __init__(self, size=0):
+        """The init methods
+
+        Args:
+            size(int): size of the square
+        """
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
@@ -8,14 +19,23 @@ class Square:
         self.__size = size
 
     def area(self):
+        """This method returnsthe area of hte square
+        """
         return (self.__size * self.__size)
 
     @property
     def size(self):
+        """int: this is the size of the square
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """size setter method
+
+        Args:
+            value(int): the size
+        """
         if type(value) != int:
             raise TypeError("size must be an integer")
             return
@@ -25,6 +45,8 @@ class Square:
         self.__size = value
 
     def my_print(self):
+        """This method prints the square.
+        """
         if self.size is 0:
             print("")
         for i in range(0, self.size):

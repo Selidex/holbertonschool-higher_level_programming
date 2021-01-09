16a025a96c7057d7dc6cbeb9f764e4912fdda4b3
Selidex <justindparnell@yahoo.com>
2021-01-08 22:11:34 -0600

    4-print_square.py

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" This module is a simple print function meant to help
+develope simple test case files"""
+
+
+def print_square(size):
+    """This function prints a square
+
+    Arg:
+        size(int): the size of the program
+    """
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+
+    for x in range(0, size):
+        for y in range(0, size):
+            print("#", end="")

cbbd2c07321e764a777ed65145e9b2dbb8546bfd
Selidex <justindparnell@yahoo.com>
2021-01-08 18:52:05 -0600

    Pushing for transfer to new device

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" This module is a simple add interger function meant to learn how to
+develope simple test case files"""
+
+
+def add_integer(a, b=98):
+    """Simple addition function
+
+    Args:
+        a (int): the first number
+        b (int): the second number, defaults to 98
+    """
+    
+    return a + b

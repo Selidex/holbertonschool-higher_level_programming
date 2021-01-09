cff738429190d6490f58986d234ae05175661a1f
Selidex <justindparnell@yahoo.com>
2021-01-08 23:54:41 -0600

    6-max_integer

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """This class tests the max integer function
+    """
+    def TestAllNumber(self):
+        # Tests to see if it finds proper max, assuming all variables are ints
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+        self.assertEqual(max_integer([2, 1, 4, 3]), 4)
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer([98]), 98)
+
+    def TestType(self):
+        # Tests to make sure proper types are used
+        self.assertRaises(TypeError, max_integer, 2)
+        self.assertRaises(TypeError, max_integer, "Bob Ross")
+        self.assertRaises(TypeError, max_integer, ["Pie", "Chocolate", "Zebr"])
+        self.assertRaises(TypeError, max_integer, None)

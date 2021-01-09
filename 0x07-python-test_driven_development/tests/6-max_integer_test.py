bad8e96e591e48dbaae180ef74e8f29a18f21089
Selidex <justindparnell@yahoo.com>
2021-01-09 00:26:48 -0600

    changed name to exception

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -20,5 +20,5 @@ class TestMaxInteger(unittest.TestCase):
     def test_Type(self):
         """ Tests to make sure proper types are used """
         self.assertRaises(TypeError, max_integer, 2)
-        self.assertRaises(TypeError, max_integer, ["Pie", "Chocolate", "Zebr"])
+        self.assertRaises(Exception, max_integer, ["Pie", "Chocolate", "Zebr"])
         self.assertRaises(TypeError, max_integer, None)

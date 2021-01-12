2ad0e314aa23abd7d2bfd9d648b9c22d0a5f2332
Selidex <justindparnell@yahoo.com>
2021-01-11 18:30:05 -0600

    Documentation upgrade

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -3,7 +3,7 @@
 
 
 class Rectangle:
-    # This is a rectangle class
+    """ This is a rectangle class"""
     def __init__(self, width=0, height=0):
         """ Initializes the rectangle.
 
@@ -22,7 +22,7 @@ class Rectangle:
         self.__height = height
 
     def __str__(self):
-        # Returns a string representation of the class
+        """ Returns a string representation of the class"""
         if self.__width is 0 or self.__height is 0:
             return ""
         else:
@@ -30,12 +30,12 @@ class Rectangle:
             return string[:-1]
 
     def __repr__(self):
-        # Returns official string rep of the class
+        """ Returns official string rep of the class"""
         return 'Rectangle(%s, %s)' % (self.width, self.height)
 
     @property
     def width(self):
-        # int: width of rectangle
+        """ int: width of rectangle"""
         return self.__width
 
     @width.setter
@@ -52,7 +52,7 @@ class Rectangle:
 
     @property
     def height(self):
-        # int: height of rectangle
+        """ int: height of rectangle"""
         return self.__height
 
     @height.setter
@@ -68,11 +68,11 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        # Returns area of the rectangle
+        """ Returns area of the rectangle"""
         return self.__width * self.__height
 
     def perimeter(self):
-        # Returns perimeter of a rectangle.
+        """ Returns perimeter of a rectangle."""
         if self.__width is 0 or self.__height is 0:
             return 0
         else:

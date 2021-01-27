06744dfa0396c76c4f69331d34b60c845a90d18e
Selidex <justindparnell@yahoo.com>
2021-01-27 03:31:44 +0000

    Updating tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -12,9 +12,11 @@ class TestRectangle(unittest.TestCase):
         r1 = Rectangle(10, 2)
         self.assertEqual(r1.width, 10)
         self.assertEqual(r1.height, 2)
+        self.assertEqual(r1.id, 4)
         self.assertEqual(r1.x, 0)
         self.assertEqual(r1.y, 0)
         self.assertEqual(r1.area(), 20)
+        self.assertEqual(r1.__str__(), "[Rectangle] (4) 0/0 - 10/2")
         r2 = Rectangle(10, 2, 1, 3, 12)
         self.assertEqual(r2.id, 12)
         self.assertEqual(r2.width, 10)
@@ -22,7 +24,7 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r2.x, 1)
         self.assertEqual(r2.y, 3)
 
-    def test_taskthree(self):
+    def test_validators(self):
         """Tests for int validators"""
         self.assertRaises(TypeError, Rectangle, "2", 2)
         self.assertRaises(TypeError, Rectangle, 2.5)

b0cb749af07ca95d2053bad63df61729ebc311e2
Selidex <justindparnell@yahoo.com>
2021-01-27 02:19:09 +0000

    updating more tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -10,16 +10,14 @@ class TestRectangle(unittest.TestCase):
     def test_tasktwo(self):
         """Tests task two functions"""
         r1 = Rectangle(10, 2)
-        self.assertEqual(r1.id, 1)
         self.assertEqual(r1.width, 10)
         self.assertEqual(r1.height, 2)
         self.assertEqual(r1.x, 0)
         self.assertEqual(r1.y, 0)
+        self.assertEqual(r1.id, 1)
         r2 = Rectangle(10, 2, 1, 3, 12)
         self.assertEqual(r2.id, 12)
         self.assertEqual(r2.width, 10)
         self.assertEqual(r2.height, 2)
         self.assertEqual(r2.x, 1)
         self.assertEqual(r2.y, 3)
-        r3 = Rectangle(11, 2)
-        self.assertEqual(r3.id, 2)
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -8,4 +8,15 @@ from models.square import Square
 class TestSquare(unittest.TestCase):
     """This class tests Square methods"""
     def test_taskten(self):
-        
+        s1 = Square(1)
+        self.assertEqual(s1.id, 1)
+        self.assertEqual(s1.size, 1)
+        self.assertEqual(s1.width, 1)
+        self.assertEqual(s1.height, 1)
+        self.assertEqual(s1.x, 0)
+        self.assertEqual(s1.y, 0)
+        s2 = Square(1, 2, 3, 12)
+        self.assertEqual(s2.id, 12)
+        self.assertEqual(s2.size, 1)
+        self.assertEqual(s2.x, 2)
+        self.assertEqual(s2.y, 3)

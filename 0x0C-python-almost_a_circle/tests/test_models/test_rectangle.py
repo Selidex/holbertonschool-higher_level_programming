dc86832903ec8cc9cb2500b1813b771a0a8589cc
Selidex <justindparnell@yahoo.com>
2021-01-27 03:17:51 +0000

    Updating more tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -7,17 +7,31 @@ from models.rectangle import Rectangle
 
 class TestRectangle(unittest.TestCase):
     """This class tests rectangle"""
-    def test_tasktwo(self):
-        """Tests task two functions"""
+    def test_attributes(self):
+        """Tests rectanlge attributes"""
         r1 = Rectangle(10, 2)
         self.assertEqual(r1.width, 10)
         self.assertEqual(r1.height, 2)
         self.assertEqual(r1.x, 0)
         self.assertEqual(r1.y, 0)
-        self.assertEqual(r1.id, 1)
+        self.assertEqual(r1.area(), 20)
         r2 = Rectangle(10, 2, 1, 3, 12)
         self.assertEqual(r2.id, 12)
         self.assertEqual(r2.width, 10)
         self.assertEqual(r2.height, 2)
         self.assertEqual(r2.x, 1)
         self.assertEqual(r2.y, 3)
+
+    def test_taskthree(self):
+        """Tests for int validators"""
+        self.assertRaises(TypeError, Rectangle, "2", 2)
+        self.assertRaises(TypeError, Rectangle, 2.5)
+        self.assertRaises(TypeError, Rectangle, 2, "2")
+        self.assertRaises(TypeError, Rectangle, 2, 2, "2")
+        self.assertRaises(TypeError, Rectangle, 2, 2, 2, "2")
+        self.assertRaises(ValueError, Rectangle, 0, 1)
+        self.assertRaises(ValueError, Rectangle, 1, 0)
+        self.assertRaises(ValueError, Rectangle, -1, 1)
+        self.assertRaises(ValueError, Rectangle, 1, -1)
+        self.assertRaises(ValueError, Rectangle, 1, 1, -1)
+        self.assertRaises(ValueError, Rectangle, 1, 1, 0, -1)
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -9,12 +9,12 @@ class TestSquare(unittest.TestCase):
     """This class tests Square methods"""
     def test_taskten(self):
         s1 = Square(1)
-        self.assertEqual(s1.id, 1)
         self.assertEqual(s1.size, 1)
         self.assertEqual(s1.width, 1)
         self.assertEqual(s1.height, 1)
         self.assertEqual(s1.x, 0)
         self.assertEqual(s1.y, 0)
+        self.assertEqual(s1.area(), 1)
         s2 = Square(1, 2, 3, 12)
         self.assertEqual(s2.id, 12)
         self.assertEqual(s2.size, 1)

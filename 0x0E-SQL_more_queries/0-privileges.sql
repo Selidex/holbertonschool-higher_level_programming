3d8bc3aedf7c492c4ac756c73ea90e30b136f198
Selidex <justindparnell@yahoo.com>
2021-03-03 00:51:29 +0000

    0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
 -- shows user permissions
--- random comment testing
-SHOW GRANTS FOR 'user_0d_1'@'local_host';
-SHOW GRANTS FOR 'user_0d_2'@'local_host';
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost

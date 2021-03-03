bc70a0c5186a83a686587d3369596b7874467243
Selidex <justindparnell@yahoo.com>
2021-03-03 01:49:58 +0000

    8-cities_of_california_subquery.sql

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,4 @@
+-- lists all cities from california
+-- temporary comment due to requirements
+SET @v1 := (SELECT id FROM states WHERE name = "California";
+SELECT id, name FROM cities WHERE state_id = @v1;

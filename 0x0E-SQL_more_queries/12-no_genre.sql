cc47fcd7b97746beeff8bfa6aa9bb8763eea793e
Selidex <justindparnell@yahoo.com>
2021-03-03 04:25:07 +0000

    Adding comments

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,6 @@
 -- shows all show genres
 -- shows NULL if no id given
+-- doing a comment test
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id

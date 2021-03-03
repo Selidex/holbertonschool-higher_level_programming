287b856b72e603c8e306bc08a3a827805f1d5ef2
Selidex <justindparnell@yahoo.com>
2021-03-03 01:39:09 +0000

    7-cities.sql

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- creates a database and a table
+-- creates primary and foreign keys
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+id INT NOT NULL AUTO_INCREMENT,
+state_id INT NOT NULL,
+name VARCHAR(256),
+UNIQUE(id),
+PRIMARY KEY(id),
+FOREIGN KEY(state_id) REFERENCES states(id))

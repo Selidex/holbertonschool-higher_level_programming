7bde663248a777adcad7acc07276a582b0425521
Selidex <justindparnell@yahoo.com>
2021-03-22 22:44:39 +0000

    Printing X's instead of #'s

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@ if (isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
   let i = 0;
-  const s = '#';
+  const s = 'X';
   for (i = 0; i < Number(process.argv[2]); i++) {
     console.log(s.repeat(Number(process.argv[2])));
   }

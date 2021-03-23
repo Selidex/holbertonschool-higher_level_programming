852bbec0119b8a27e20594051f9dcbc542b1b013
Selidex <justindparnell@yahoo.com>
2021-03-23 21:36:29 +0000

    7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,8 +3,7 @@ exports.nbOccurences = function (list, searchElement) {
   let n = 0;
   let i = 0;
   for (i = 0; i < list.length; i++) {
-    if (list[i] === searchElement)
-      { n++; }
+    if (list[i] === searchElement) { n++; }
   }
   return n;
 };

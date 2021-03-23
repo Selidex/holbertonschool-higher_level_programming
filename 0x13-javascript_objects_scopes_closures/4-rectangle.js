c2cfa3c4db07b952f6935a5463463fcba81010ea
Selidex <justindparnell@yahoo.com>
2021-03-23 20:48:32 +0000

    4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let x = 0;
+    const s = 'X';
+    for (x = 0; x < this.height; x++) {
+      console.log(s.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const tmp = this.height;
+    this.height = this.width;
+    this.width = tmp;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+}
+module.exports = Rectangle;

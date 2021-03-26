eeb926bb74a5dbad594c8243d51eef88fd44e3c4
Selidex <justindparnell@yahoo.com>
2021-03-26 22:51:32 +0000

    2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+MY_HOST = "localhost"
+MY_USER = sys.argv[1]
+MY_PASS = sys.argv[2]
+MY_DB = sys.argv[3]
+test = sys.argv[4]
+db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASS, db=MY_DB)
+cur = db.cursor()
+cur.execute("SELECT id, name FROM states WHERE name = '{}' \
+ORDER BY id ASC".format(test))
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+db.close()

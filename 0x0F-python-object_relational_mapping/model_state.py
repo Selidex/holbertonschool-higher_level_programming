63fc9dd09e87b9595c6282032651328a8cfc01fe
Selidex <justindparnell@yahoo.com>
2021-03-27 20:45:06 +0000

    model_state.py

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Base model declarations"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+class State(Base):
+    """The state class for my database"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)

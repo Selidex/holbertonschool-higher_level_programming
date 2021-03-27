0659f2959a636498ce17527c7e9ce45a29b79aeb
Selidex <justindparnell@yahoo.com>
2021-03-27 23:35:21 +0000

    all your base our belong to us

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 """Base model declarations"""
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base, State
-
-Base = declarative_base()
 
 
 class City(Base):

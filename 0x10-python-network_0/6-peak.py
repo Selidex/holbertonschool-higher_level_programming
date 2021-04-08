4336b95ab7506ad2fa33e8fbda50f0a0cf3ba97c
Selidex <justindparnell@yahoo.com>
2021-04-08 22:11:46 +0000

    pep8

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 """ Test function find_peak """
 
+
 def find_peak(list_of_integers):
-   """Tests the peak of the list"""
-   loi = list_of_integers
-   if loi is None or len(loi) == 0:
-      return None
-   start = 0
-   end = len(loi) - 1
-   while start < end:
-      mid = start + (end - start) // 2
-      if loi[mid] > loi[mid - 1] and loi[mid] > loi[mid + 1]:
-         return loi[mid]
-      if loi[mid - 1] > loi[mid + 1]:
-         end = mid
-      else:
-         start = mid + 1
-   return loi[start]
+    """Tests the peak of the list"""
+    loi = list_of_integers
+    if loi is None or len(loi) == 0:
+        return None
+    start = 0
+    end = len(loi) - 1
+    while start < end:
+        mid = start + (end - start) // 2
+        if loi[mid] > loi[mid - 1] and loi[mid] > loi[mid + 1]:
+            return loi[mid]
+        if loi[mid - 1] > loi[mid + 1]:
+            end = mid
+        else:
+            start = mid + 1
+    return loi[start]
